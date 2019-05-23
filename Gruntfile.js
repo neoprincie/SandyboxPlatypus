39268a216432ffa59fe90a597802504e4c9389ac
Bryan Prince <bryan.prince@Bryans-MacBook-Pro.local>
2019-05-23 12:46:22 -0400

    Made local work

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,103 @@
-module.exports = function (grunt) {
-	"use strict";
-	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
-	grunt.config.merge({ compatVersion: "edge" });
-	grunt.registerTask("default", [
-		"clean",
-		"lint",
-		"build"
-	]);
-	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-test");
-	grunt.registerTask("unit_and_integration_tests", ["test"]);
-	grunt.config.merge({
-		coverage_threshold: {
-			statements: 0,
-			branches: 100,
-			functions: 0,
-			lines: 0
+'use strict';
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		connect: {
+			options: {
+				port: 8080,
+				hostname: '*'
+			},
+			src: {},
+			dist: {}
+		},
+
+		openui5_connect: {
+			options: {
+				resources: [
+					'sdk/sapui5-sdk-1.65.1/resources',
+				],
+				testresources: [
+					'sdk/sapui5-sdk-1.65.1/test-resources',
+				]
+			},
+			src: {
+				options: {
+					appresources: 'webapp'
+				}
+			},
+			dist: {
+				options: {
+					appresources: 'dist'
+				}
+			}
+		},
+
+		openui5_preload: {
+			component: {
+				options: {
+					resources: {
+						cwd: 'webapp',
+						prefix: 'simple-app',
+						src: [
+							'**/*.js',
+							'**/*.fragment.html',
+							'**/*.fragment.json',
+							'**/*.fragment.xml',
+							'**/*.view.html',
+							'**/*.view.json',
+							'**/*.view.xml',
+							'**/*.properties',
+							'manifest.json',
+							'!test/**'
+						]
+					},
+					dest: 'dist'
+				},
+				components: true
+			}
+		},
+
+		clean: {
+			dist: 'dist',
+			coverage: 'coverage'
+		},
+
+		copy: {
+			dist: {
+				files: [ {
+					expand: true,
+					cwd: 'webapp',
+					src: [
+						'**',
+						'!test/**'
+					],
+					dest: 'dist'
+				} ]
+			}
+		},
+
+		eslint: {
+			webapp: ['webapp']
 		}
+
 	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-openui5');
+
+	// Server task
+	grunt.registerTask('serve', function(target) {
+		grunt.task.run('openui5_connect:' + (target || 'src') + ':keepalive');
+	});
+
+	// Build task
+	grunt.registerTask('build', ['clean:dist', 'openui5_preload', 'copy']);
+
+	// Default task
+	grunt.registerTask('default', ['serve']);
 };
